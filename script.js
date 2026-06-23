204ba33910e6010ae6361fc81527f487c93d6e0d
Hero <sovereign.university20@gmail.com>
2026-06-22 22:17:17 -0500

    api fetch and submit working

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 // so much empty
+const postList = document.getElementById("postList");
+const postForm = document.getElementById("postForm");
+const formError = document.getElementById("formError");
+const formSuccess = document.getElementById("formSuccess");
+const fetchButton = document.getElementById("fetchButton");
+const errorOnPage = document.getElementById("error");
+const titleInput = document.getElementById("titleInput");
+const bodyInput = document.getElementById("bodyInput");
+
+fetchButton.addEventListener('click', event => {
+fetch("https://jsonplaceholder.typicode.com/posts")  
+.then(response => {
+        return response.json();
+    })
+    .then(data => { 
+data.forEach(newPost => { 
+const newPostTitle = document.createElement('h1');
+const newPostBody = document.createElement('p');
+newPostTitle.innerHTML = newPost.title;
+newPostBody.innerHTML = newPost.body;
+postList.appendChild(newPostTitle);
+postList.appendChild(newPostBody);
+})
+    })
+    .catch(function (error) {
+        console.error("Error fetching data", error);
+    })
+});
+
+postForm.addEventListener('submit', event => {
+    event.preventDefault()
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({title: titleInput.value, body:bodyInput.value}),
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        formSuccess.textContent = (`Submission successful, ${titleInput.value} added.`)
+    })
+    .catch(function (error) {
+        console.error("Error fetching data", error);
+    })
+    
+});
+
+
